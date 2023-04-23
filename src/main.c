ad5363645ce6c0e4a8f9ae0a30728eadd68298a3
Timothy Cronin <tcroniniv@gmail.com>
2023-04-23 13:29:29 -0400

    add license to code

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-only
+// AUTHOR: Timothy Cronin
 #include <stdio.h>
 
-int main()
-{
+int main() {
+  //Order of the iris.data file
+  // Sepal Length, Sepal Width, Petal Length, Petal Width, Species
   printf("Hello World\n");
 
   return 0;
