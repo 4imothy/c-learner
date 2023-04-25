2a83c018a0e8d546590544d441038a78fc3ee6e7
Timothy Cronin <tcroniniv@gmail.com>
2023-04-25 16:43:53 -0400

    parse the csv fully

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+// TODO read the full CSV and make the data points the valid types
+
 int main()
 {
     // Order of the iris.data file
@@ -25,20 +27,51 @@ int main()
     }
     char *buffer = NULL;
     size_t bufsize = 0;
-    // read the header
-    // if getline(&buffer, &bufsize, myFile) != -1 {
-    //     header = strok(buffer, ",");
-    // }
 
-    // read the remaining
+    getline(&buffer, &bufsize, myFile);
+    char **header = NULL;
+    char *token = strtok(buffer, ",");
+    int num_cols = 0;
+    // get the header
+    while (token) {
+        num_cols++;
+        header = realloc(header, num_cols * sizeof(char *));
+        header[num_cols - 1] = token;
+        token = strtok(NULL, ",");
+    }
+
+    char ***lines= NULL;
+    int num_lines = 0;
     while (getline(&buffer, &bufsize, myFile) != -1) {
-        // printf("%d\n", num_columns(buffer));
-        char *token = strtok(buffer, ",");
+        token = strtok(buffer, ",");
+        // while there are more tokens in this line
         while (token) {
-            // printf("%s", token);
-            // token = strtok(NULL, ",");
+            int len = strlen(token);
+            char **line = malloc(num_cols * sizeof(char *));
+            int j = 0;
+            while (token[len - 1] != '\n'){
+                line[j] = malloc(strlen(token) + 1);
+                strcpy(line[j], token);
+                token = strtok(NULL, ",");
+                len = strlen(token);
+                j++;
+            }
+            // the current token is the target
+            line[j] = malloc(strlen(token) + 1);
+            strcpy(line[j], token);
+            lines = realloc(lines, num_cols * (num_lines+1) * sizeof(char *));
+            lines[num_lines] = line;
+            num_lines++;
+            token = strtok(NULL, ",");
         }
     }
     free(buffer);
+    free(token);
+
+    int tar_idx = num_cols - 1;
+    for (int i = 0; i < num_lines; i++){
+        printf("%s", lines[i][tar_idx]);
+    }
+    
     return 0;
 }
