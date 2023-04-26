a3d5ac2c8a8d55da00b09cd5fb2c4f8ec98bfec3
Timothy Cronin <tcroniniv@gmail.com>
2023-04-26 13:35:44 -0400

    better formatting

--- src/csv_lexer/csv_lexer.h
+++ src/csv_lexer/csv_lexer.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double ***lex_csv(FILE *file, char*** classes, int *num_data, int *num_cols, int *num_classes);
+double ***lex_csv(FILE *file, char ***classes, int *num_data, int *num_cols,
+                  int *num_classes);
 
 #endif
