1f0cb7a27f27f5ddf132d09ef1751680227edecb
Timothy Cronin <tcroniniv@gmail.com>
2023-04-26 13:38:08 -0400

    cleanup

--- src/obj_funcs/objective_functions.c
+++
@@ -1,9 +0,0 @@
-#include "objective_functions.h"
-#include <stdio.h>
-
-float mse(float predictions[], float targets[])
-{
-    printf("%f\n", predictions[0]);
-    printf("%f\n", targets[0]);
-    return 1.0;
-}
--- src/obj_funcs/objective_functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef OBJECTIVE_FUNCTIONS_H
-#define OBJECTIVE_FUNCTIONS_H
-
-float mse(float predictions[], float targets[]);
-
-#endif
