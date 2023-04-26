26d5c39a4301b256bcf2e57d0f9c297db1411f25
Timothy Cronin <tcroniniv@gmail.com>
2023-04-26 13:35:56 -0400

    implement training

--- src/main.c
+++ src/main.c
@@ -4,19 +4,33 @@
 #include "csv_lexer/csv_lexer.h"
 #include "obj_funcs/objective_functions.h"
 #include "utils/utils.h"
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
-#include <time.h> 
-#include <math.h>
+#include <string.h>
+#include <time.h>
 
 #define LEARNING_RATE 0.1
 
-double sigmoid(double y) {
-    return 1 / (1 + exp(-1 * y));
+void update(double ***data, double *weights, double *predictions, int num_data,
+            int input_dim)
+{
+    double coef = (-2 * LEARNING_RATE) / num_data;
+    for (int i = 0; i <= input_dim; i++) {
+        double sum = 0;
+        for (int j = 0; j < num_data; j++) {
+            sum += ((*data[j][input_dim - 1] / 3) - predictions[j]) *
+                   (1 - predictions[j]) * predictions[j] * (*data[j][i] / 3);
+        }
+        weights[i] -= (sum *= coef);
+    }
 }
 
-double predict(double *weights, double **point, int input_dim) {
+double sigmoid(double y) { return 1 / (1 + exp(-1 * y)); }
+
+double predict(double *weights, double **point, int input_dim)
+{
     double y = 0;
     int i = 0;
     for (; i < input_dim; i++) {
@@ -28,19 +42,21 @@ double predict(double *weights, double **point, int input_dim) {
     return sigmoid(y);
 }
 
-
-double evaluate_model(double *weights, double ***data, double *predictions, int num_data, int input_dim, int num_classes) {
+double evaluate_model(double *weights, double ***data, double *predictions,
+                      int num_data, int input_dim, int num_classes)
+{
     double mse = 0;
     for (int i = 0; i < num_data; i++) {
         double prediction = predict(weights, data[i], input_dim);
+        predictions[i] = prediction;
         // multiply by number of classes
         double target = *data[i][input_dim];
         mse += pow((target / num_classes) - prediction, 2.0);
-        predictions[i] = prediction;
     }
 
-    return mse / num_data;
+    return (mse / num_data);
 }
+
 int main()
 {
     // Order of the iris.data file
@@ -57,29 +73,37 @@ int main()
     int num_cols = 0;
     int num_classes = 0;
     char **classes = NULL;
-    double ***data = lex_csv(file, &classes, &num_data, &num_cols, &num_classes);
+    double ***data =
+        lex_csv(file, &classes, &num_data, &num_cols, &num_classes);
     int input_dim = num_cols - 1; // last one is target
     double weights[input_dim];
     srand(time(NULL)); // set the seed value based on current time
-    // srand(1);
     // add 1 for the bias
-    for (int i = 0; i < input_dim + 1; i++) {
-        weights[i] = ((double)rand()/(double)(RAND_MAX));
+    for (int i = 0; i <= input_dim + 1; i++) {
+        weights[i] = ((double)rand() / (double)(RAND_MAX));
     }
 
-    double predictions[num_data];
-    double mse = evaluate_model(weights, data, predictions, num_data, input_dim, num_classes);
-    printf("Error: %f\n", mse);
+    bool conv = false;
+    while (!conv) {
+        double predictions[num_data];
+        double mse = evaluate_model(weights, data, predictions, num_data,
+                                    input_dim, num_classes);
+        double delta_w[input_dim];
+        double delta_b;
+        update(data, weights, predictions, num_data, input_dim);
+
+        printf("Error: %f\n", mse);
+        if (mse < 0.04) {
+            conv = true;
+        }
+    }
 
     for (int i = 0; i < num_data; i++) {
         for (int j = 0; j < num_cols; j++) {
-            // printf("%f ", *data[i][j]);
             free(data[i][j]);
         }
-        // printf("\n");
     }
     free(data);
 
     return 0;
 }
-
